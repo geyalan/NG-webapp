88123907b8391b654e5de231a7ad807e47376a8d
geyalan <490312867@qq.com>
2017-07-12 18:16:45 +0800

    add

--- build/js/index.js
+++ build/js/index.js
@@ -8,6 +8,14 @@ angular.module('app').config(['$stateProvider','$urlRouterProvider',function($st
 		url:'/main',
 		templateUrl: 'view/main.html',
 		controller:'mainCtrl'
+	}).state('position',{
+		url:'/position/:id',
+		templateUrl:'view/position.html',
+		controller:'positionCtrl'
+	}).state('company',{
+		url:'/company/:id',
+		templateUrl:'view/company.html',
+		controller:'companyCtrl'
 	});
 	$urlRouterProvider.otherwise('main');
 }])
@@ -21,66 +29,39 @@ angular.module('app').config(['$stateProvider','$urlRouterProvider',function($st
 // 	$urlRouterProvider.otherwise('main')
 // })
 'use strict';
-angular.module('app').controller('mainCtrl',['$scope',function($scope){
-	$scope.list = [{
-    "id": "p3",
-    "imageUrl": "/image/company-3.png",
-    "job": "销售",
-    "companyId": "c3",
-    "companyName": "千度",
-    "cityId": "c2",
-    "cityName": "上海",
-    "scaleId": "s4",
-    "scaleName": "500人以上",
-    "industryId": "i1",
-    "industryName": "互联网",
-    "salaryId": "s2",
-    "salaryName": "3k-5k",
-    "experience": "1-3年",
-    "education": "专科",
-    "benefit": "成长空间大",
-    "description": "岗位职责：\n1.销售产品；...",
-    "date": "2016-06-01 11:05"
-  }, {
-    "id": "p4",
-    "imageUrl": "/image/company-3.png",
-    "job": "产品经理",
-    "companyId": "c3",
-    "companyName": "千度",
-    "cityId": "c3",
-    "cityName": "广州",
-    "scaleId": "s4",
-    "scaleName": "500人以上",
-    "industryId": "i1",
-    "industryName": "互联网",
-    "salaryId": "s6",
-    "salaryName": "15k-25k",
-    "experience": "5年以上",
-    "education": "本科",
-    "benefit": "公司氛围好",
-    "description": "岗位职责：\n1.制定运营可行的方案；...",
-    "date": "2016-05-01 11:05"
-  },{
-    "id": "p1",
-    "imageUrl": "/image/company-1.png",
-    "job": "WEB前端",
-    "companyId": "c1",
-    "companyName": "慕课网",
-    "cityId": "c1",
-    "cityName": "北京",
-    "scaleId": "s2",
-    "scaleName": "50-100人",
-    "industryId": "i1",
-    "industryName": "互联网",
-    "salaryId": "s5",
-    "salaryName": "10k-15k",
-    "experience": "1-3年",
-    "education": "本科",
-    "benefit": "发展前景好",
-    "description": "岗位职责：\n1.负责公司前端页面开发；...",
-    "date": "2016-06-01 01:05"
-  }]
+angular.module('app').controller('companyCtrl',['$scope',function($scope){
 }]);
+'use strict';
+angular.module('app').controller('mainCtrl',['$http','$scope',function($http,$scope){
+	$http.get('/data/positionList.json').then(function(resp){
+        $scope.list = resp.data
+    })
+}]);
+
+
+
+
+'use strict';
+angular.module('app').controller('positionCtrl',['$scope',function($scope){
+}]);
+// 'use strict';
+// angular.module('app').directive('appCompany',[function(){
+// 	return{
+// 		restrict:'A',
+// 		replace:true,
+// 		tempalteUrl:'view/tempalte/company.html'
+// 	}
+// }])
+
+'use strict';
+angular.module('app').directive('appCompany', [function(){
+  return {
+    restrict: 'A',
+    replace: true,
+    templateUrl: 'view/template/company.html'
+  };
+}]);
+
 'use strict';
  angular.module('app').directive('appFoot',[function(){
  	return {
@@ -96,6 +77,44 @@ angular.module('app').directive('appHead',[function(){
 		replace:true,
 		templateUrl:'view/template/head.html'
 	};
+}])
+'use strict';
+angular.module('app').directive('appHeadBar',[function(){
+	return {
+		restrict:'A',
+		replace:true,
+		templateUrl:'view/template/headBar.html',
+		scope:{
+			text:'@'
+		},
+		link:function($scope){
+			$scope.back = function(){
+				window.history.back();
+			}
+		}
+	}
+}])
+'use strict';
+angular.module('app').directive('appPositionClass',[function(){
+	return{
+		restrict:'A',
+		replace:true,
+		templateUrl:'view/template/positionClass.html'
+	}
+}])
+'use strict';
+angular.module("app").directive("appPositionInfo",[function(){
+	return {
+		restrict:'A',
+		replace:true,
+		templateUrl:'view/template/positionInfo.html',
+		scope:{
+			isActive:'='
+		},
+		link:function($scope){
+			$scope.imagePath = $scope.isActive?'image/star-active.png':'image/star.png'
+		}
+	}
 }])
 'use strict';
 angular.module('app').directive('appPositionList',[function(){
---
+++ src/script/controller/mainCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+angular.module('app').controller('mainCtrl',['$http','$scope',function($http,$scope){
+	$http.get('/data/positionList.json').then(function(resp){
+        $scope.list = resp.data
+    })
+}]);
+
+
+
